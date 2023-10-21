01d10dac413d9b94cfb7959bceda012f9ab26b4a
wikuskina <wikuskina@gmail.com>
2023-10-21 05:06:31 +0100

    While loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        // User entry
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter day of the week in English. It will be translated to Russian.");
-        String entry = in.nextLine();
-
         // Days of Week
         HashMap<String, String> DaysList = new HashMap<String, String>();
         DaysList.put("Monday", "Понедельник");
@@ -20,12 +14,18 @@ public class Main {
         DaysList.put("Saturday", "Суббота");
         DaysList.put("Sunday", "Воскресенье");
 
-        for (HashMap.Entry<String, String> days : DaysList.entrySet()) {
-            if (days.getKey().equals(entry)) {
-                // System output
-                System.out.println(entry + " is translated as: " + days.getValue());
+        do {
+            // User entry
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter day of the week in English. It will be translated to Russian.");
+            String entry = in.nextLine();
+
+            for (HashMap.Entry<String, String> days : DaysList.entrySet()) {
+                if (days.getKey().equals(entry)) {
+                    // System output
+                    System.out.println(entry + " is translated as: " + days.getValue());
+                }
             }
-            in.nextLine();
-        }
+        } while (true);
     }
 }
